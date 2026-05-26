4497224d2dd64345f1c90a9a1a219fa1c496dd63
Gustavo Chaves <gustavohenriquecs@unipam.edu.br>
2026-05-26 14:21:02 -0300

    Update database.py

--- agro_api/app/database.py
+++ agro_api/app/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 DB_URL = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-    f"?charset=utf8mb4&ssl_ca=/etc/ssl/certs/ca-certificates.crt"
+    f"?charset=utf8mb4&ssl_disabled=true"
 )
 
 engine = create_engine(DB_URL, echo=False)
